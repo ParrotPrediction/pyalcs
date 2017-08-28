57dcb08755315766615268bbebb9057ea58735e8
Norbert Kozlowski <khozzy@gmail.com>
2017-08-28 21:52:14 +0200

    Setup.py file updated

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_maze',
       version='0.0.1',
-      install_requires=['gym']
-)
+      description='Maze environments for OpenAI Gym Environment',
+      keywords='acs lcs machine-learning reinforcement-learning opernai',
+      url='https://github.com/ParrotPrediction/openai-maze-envs',
+      author='Parrot Prediction',
+      author_email='contact@parrotprediction.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'gym'
+      ],
+      include_package_data=False,  # We don't have other types of files
+      zip_safe=False)
