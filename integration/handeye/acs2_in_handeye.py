950870f928d7e3f5c90f45dd18c6fec73b463030
e-dzia <theladyofthegallifrey@gmail.com>
2018-08-28 09:13:40 +0200

    added handeye env (not finished) TODO: calculate_performance

---
+++ integration/handeye/acs2_in_handeye.py
@@ -0,0 +1,38 @@
+import logging
+
+import gym
+
+import sys
+sys.path.append('/home/e-dzia/openai-envs')
+
+# noinspection PyUnresolvedReferences
+import gym_handeye
+from alcs import ACS2, ACS2Configuration
+from integration.handeye.utils import calculate_performance
+
+# Configure logger
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == '__main__':
+
+    # Load desired environment
+    hand_eye = gym.make('HandEye3-v0')
+
+    # Configure and create the agent
+    cfg = ACS2Configuration(10, 6,
+                            epsilon=1.0,
+                            do_ga=False,
+                            performance_fcn=calculate_performance)
+    logging.info(cfg)
+
+    # Explore the environment
+    agent = ACS2(cfg)
+    population, explore_metrics = agent.explore(hand_eye, 50)
+
+    # Exploit the environment
+    agent = ACS2(cfg, population)
+    population, exploit_metric = agent.exploit(hand_eye, 10)
+
+    for metric in exploit_metric:
+        logging.info(metric)
---
+++ integration/handeye/utils.py
@@ -0,0 +1,32 @@
+def calculate_performance(maze, population):
+    """
+    Analyzes all possible transition in maze environment and checks if there
+    is a reliable classifier for it.
+    :param maze: maze object
+    :param population: list of classifiers
+    :return: percentage of knowledge
+    """
+    # transitions = maze.env.get_all_possible_transitions()
+    #
+    # # Take into consideration only reliable classifiers
+    # reliable_classifiers = [c for c in population if c.is_reliable()]
+    #
+    # # Count how many transitions are anticipated correctly
+    # nr_correct = 0
+    #
+    # # For all possible destinations from each path cell
+    # for start, action, end in transitions:
+    #
+    #     p0 = maze.env.maze.perception(*start)
+    #     p1 = maze.env.maze.perception(*end)
+    #
+    #     if any([True for cl in reliable_classifiers
+    #             if cl.predicts_successfully(p0, action, p1)]):
+    #         nr_correct += 1
+    #
+    # return {
+    #     'knowledge': nr_correct / len(transitions) * 100.0
+    # }
+    return {
+        'knowledge': 1
+    }
