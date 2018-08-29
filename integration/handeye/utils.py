4eb972b5d3b982e95ec09990ce9ebdc2a0d63a06
e-dzia <theladyofthegallifrey@gmail.com>
2018-08-29 16:07:07 +0200

    changed calculate_performance function But it doesn't work well still :/

--- integration/handeye/utils.py
+++ integration/handeye/utils.py
@@ -1,32 +1,29 @@
-def calculate_performance(maze, population):
+def calculate_performance(hand_eye, population):
     """
     Analyzes all possible transition in maze environment and checks if there
     is a reliable classifier for it.
-    :param maze: maze object
+    :param hand_eye: maze object
     :param population: list of classifiers
     :return: percentage of knowledge
     """
-    # transitions = maze.env.get_all_possible_transitions()
-    #
-    # # Take into consideration only reliable classifiers
-    # reliable_classifiers = [c for c in population if c.is_reliable()]
-    #
-    # # Count how many transitions are anticipated correctly
-    # nr_correct = 0
-    #
-    # # For all possible destinations from each path cell
-    # for start, action, end in transitions:
-    #
-    #     p0 = maze.env.maze.perception(*start)
-    #     p1 = maze.env.maze.perception(*end)
-    #
-    #     if any([True for cl in reliable_classifiers
-    #             if cl.predicts_successfully(p0, action, p1)]):
-    #         nr_correct += 1
-    #
-    # return {
-    #     'knowledge': nr_correct / len(transitions) * 100.0
-    # }
+    transitions = hand_eye.env.get_all_possible_transitions()
+
+    # Take into consideration only reliable classifiers
+    reliable_classifiers = [c for c in population if c.is_reliable()]
+
+    # Count how many transitions are anticipated correctly
+    nr_correct = 0
+
+    # For all possible destinations from each path cell
+    for start, action, end in transitions:
+
+        p0 = start
+        p1 = end
+
+        if any([True for cl in reliable_classifiers
+                if cl.predicts_successfully(p0, action, p1)]):
+            nr_correct += 1
+
     return {
-        'knowledge': 1
+        'knowledge': nr_correct / len(transitions) * 100.0
     }
