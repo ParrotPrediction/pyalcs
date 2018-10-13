9bc28abb57d1660bd10247395b9cff341a9306f4
Michał Szymański <smiszym@gmail.com>
2018-10-13 17:44:34 +0200

    Proper usage of ansi render mode of Multiplexer

--- examples/acs2/boolean_multiplexer/utils.py
+++ examples/acs2/boolean_multiplexer/utils.py
@@ -2,7 +2,7 @@ from gym_multiplexer.utils import get_correct_answer
 
 
 def calculate_performance(env, population, ctrl_bits=None):
-    p1 = env.render()  # state after executing action
+    p1 = env.render('ansi')  # state after executing action
     p0 = p1[:-1] + ['0']  # initial state
     correct_answer = get_correct_answer(p0, ctrl_bits)  # true action
 
