273aad89c97807aaeef09229001341e3ff7ddb15
Michał Szymański <smiszym@gmail.com>
2018-11-09 13:43:57 +0100

    fixup! Give more information when raising exception

--- lcs/utils.py
+++ lcs/utils.py
@@ -1,4 +1,3 @@
 def check_types(oktypes, o):
     if not isinstance(o, oktypes):
-        raise TypeError(
-            "Wrong element type: object {}, type {}".format(o, type(o)))
+        raise TypeError(f"Wrong element type: object {o}, type {type(o)}")
