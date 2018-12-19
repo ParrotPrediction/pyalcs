745f108e7d6ee2e6f16914bb2e70fd66f6323fe3
e-dzia <theladyofthegallifrey@gmail.com>
2018-12-19 08:22:42 +0100

    added knowledge measurement

--- examples/acs2/taxi/acs2_in_taxi.py
+++ examples/acs2/taxi/acs2_in_taxi.py
@@ -1,8 +1,10 @@
 import logging
+from typing import Dict
 
 import gym
 # noinspection PyUnresolvedReferences
 import sys
+
 sys.path.append("/home/e-dzia/openai-envs/")
 import gym_taxi_goal
 
@@ -15,9 +17,57 @@ from lcs.metrics import population_metrics
 logging.basicConfig(level=logging.INFO)
 
 
+def taxi_knowledge(population, environment) -> Dict:
+    """
+    Analyzes all possible transition in taxi environment and checks if there
+    is a reliable classifier for it.
+
+    Parameters
+    ----------
+    population
+        list of classifiers
+    environment
+        taxi environment
+
+    Returns
+    -------
+    Dict
+        knowledge - percentage of transitions we are able to anticipate
+            correctly (max 100)
+    """
+    transitions = environment.env.P
+
+    # Take into consideration only reliable classifiers
+    reliable_classifiers = [c for c in population if c.is_reliable()]
+
+    # Count how many transitions are anticipated correctly
+    nr_correct = 0
+
+    # For all possible destinations from each path cell
+    for start in range(500):
+        for action in range(6):
+            local_transitions = transitions[start][action]
+            if len(local_transitions) > 1:
+                pass
+
+            prob, end, reward, done = local_transitions[0]
+
+            p0 = (str(start), )
+            p1 = (str(end), )
+
+            if any([True for cl in reliable_classifiers
+                    if cl.predicts_successfully(p0, action, p1)]):
+                nr_correct += 1
+
+    return {
+            'knowledge': nr_correct / (500.0 * 6) * 100.0
+    }
+
+
 def taxi_metrics(population, environment):
     return {
-        'population': population_metrics(population, environment)
+        'agent': population_metrics(population, environment),
+        'knowledge': taxi_knowledge(population, environment)
     }
 
 
