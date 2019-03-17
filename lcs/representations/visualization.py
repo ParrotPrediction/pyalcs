041c44deec0de971f839e0533197543183e4b3df
Norbert Kozlowski <khozzy@gmail.com>
2019-03-17 10:55:31 +0100

    Adapt the interval representation

--- lcs/representations/visualization.py
+++ lcs/representations/visualization.py
@@ -1,23 +1,31 @@
+from functools import partial
 from typing import Tuple
-
-
-def visualize(interval: Tuple[int, int],
-              val_range: Tuple[int, int],
-              n: int = 10):
-
-    range_len = len(range(val_range[0], val_range[1])) + 1
-
-    if range_len < n:
-        n = range_len
-
-    lower = _scale(interval[0], range_len, n)
-    upper = _scale(interval[1], range_len, n)
-
-    visualization = '.' * n
-    selected = 'O' * (len(range(lower, upper)) + 1)
-
-    return visualization[:lower] + selected + visualization[upper + 1:]
 
 
 def _scale(val: int, max_range: int, n: int) -> int:
     return round(n * val / max_range)
+
+
+def visualize(interval: Tuple[int, int],
+              val_range: Tuple[int, int],
+              buckets: int = 10):
+
+    def select_value(current, interval, mapped):
+        p = mapped[interval[0]]
+        q = mapped[interval[1]]
+
+        if current < p or current > q:
+            return '.'
+
+        if current in [p, q]:
+            # edges - process possible ambiguities
+            if len([v for v in mapped.values() if v == current]) > 1:
+                return 'o'
+
+        return 'O'
+
+    mapped = {val: _scale(val, max_range=val_range[1]+1, n=buckets) for
+              val in range(val_range[0], val_range[1]+1)}
+    rep = map(partial(select_value, interval=interval, mapped=mapped),
+              range(0, buckets))
+    return "".join(rep)
--- tests/lcs/representations/test_visualization.py
+++ tests/lcs/representations/test_visualization.py
@@ -4,15 +4,15 @@ from lcs.representations.visualization import visualize, _scale
 
 
 @pytest.mark.parametrize("_interval, _range, n, _visualization", [
-    ((3, 3), (0, 7), 10, '...O....'),
-    ((0, 2), (0, 7), 10, 'OOO.....'),
-    ((2, 4), (0, 7), 10, '..OOO...'),
-    ((0, 7), (0, 7), 10, 'OOOOOOOO'),
-    ((0, 1), (0, 3), 10, 'OO..'),
-    ((3, 3), (0, 3), 10, '...O'),
-    ((0, 7), (0, 15), 10, 'OOOOO.....'),
-    ((0, 15), (0, 15), 10, 'OOOOOOOOOO'),
-    ((0, 1), (0, 31), 10, 'O.........'),
+    ((0, 0), (0, 15), 10, 'O.........'),
+    ((0, 1), (0, 15), 10, 'Oo........'),
+    ((0, 2), (0, 15), 10, 'Oo........'),
+    ((0, 3), (0, 15), 10, 'OOo.......'),
+    ((0, 15), (0, 15), 10, 'OOOOOOOOOo'),
+    ((4, 13), (0, 15), 10, '..oOOOOOo.'),
+    ((5, 13), (0, 15), 10, '...OOOOOo.'),
+    ((5, 5), (0, 15), 10, '...O......'),
+    ((4, 4), (0, 15), 10, '..o.......'),
 ])
 def test_visualize(_interval, _range, n, _visualization):
     assert visualize(_interval, _range) == _visualization
