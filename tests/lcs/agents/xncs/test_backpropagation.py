39caccbb75ec29f5c8494b307928cf9012d0f2cb
Marek Chmielewski <Metron45@gmail.com>
2021-05-20 12:29:05 +0200

    BP test fix

--- tests/lcs/agents/xncs/test_backpropagation.py
+++ tests/lcs/agents/xncs/test_backpropagation.py
@@ -11,12 +11,27 @@ class TestBackpropagation:
     def cfg(self):
         return Configuration(lmc=2, lem=0.2, number_of_actions=4)
 
-    def test_init(self, cfg):
-        bp = Backpropagation(cfg)
+    @pytest.fixture
+    def situation(self):
+        return "1100"
+
+    @pytest.fixture
+    def classifiers_list_diff_actions(self, cfg, situation):
+        classifiers_list = ClassifiersList(cfg)
+        classifiers_list.insert_in_population(Classifier(cfg, Condition(situation), 0, 0, Effect(situation)))
+        classifiers_list.insert_in_population(Classifier(cfg, Condition(situation), 1, 0, Effect(situation)))
+        classifiers_list.insert_in_population(Classifier(cfg, Condition(situation), 2, 0, Effect(situation)))
+        classifiers_list.insert_in_population(Classifier(cfg, Condition(situation), 3, 0, Effect(situation)))
+        return classifiers_list
+
+    @pytest.fixture
+    def bp(self, cfg, classifiers_list_diff_actions):
+        return Backpropagation(cfg, classifiers_list_diff_actions)
+
+    def test_init(self, cfg, bp):
         assert id(bp.cfg) == id(cfg)
 
-    def test_insert(self, cfg):
-        bp = Backpropagation(cfg)
+    def test_insert(self, cfg, bp):
         cl = Classifier(cfg=cfg, condition=Condition("1111"), action=0, time_stamp=0)
         ef = Effect("0110")
         bp.insert_into_bp(cl, ef)
@@ -25,8 +40,7 @@ class TestBackpropagation:
         assert bp.classifiers_for_update[0] == cl
         assert bp.update_vectors[0] == ef
 
-    def test_update(self, cfg):
-        bp = Backpropagation(cfg)
+    def test_update(self, cfg, bp):
         cl = Classifier(cfg=cfg, condition=Condition("1111"), action=0, time_stamp=0)
         ef = Effect("0110")
         bp.insert_into_bp(cl, ef)
@@ -37,12 +51,3 @@ class TestBackpropagation:
         assert cl.effect == ef
         assert cl.error != cfg.initial_error
 
-    def test_update(self, cfg):
-        bp = Backpropagation(cfg)
-        cl = Classifier(cfg=cfg, condition=Condition("1111"), action=0, time_stamp=0)
-        ef = Effect("0110")
-        bp.insert_into_bp(cl, ef)
-        bp.check_and_update()
-        assert cl.effect is None
-        bp.check_and_update()
-        assert cl.effect is not None
