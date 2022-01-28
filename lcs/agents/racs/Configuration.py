c1003843aa9550159cc9a5136bd7d8624dbeac43
Norbert Kozłowski <nkozlowski@protonmail.com>
2022-01-28 18:08:46 +0100

    feat(racs): new configuration syntax

--- lcs/agents/racs/Configuration.py
+++ lcs/agents/racs/Configuration.py
@@ -1,66 +1,44 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from lcs.representations import UBR
 
 
 class Configuration:
-    def __init__(self,
-                 classifier_length: int,
-                 number_of_possible_actions: int,
-                 encoder=None,
-                 user_metrics_collector_fcn: Callable = None,
-                 metrics_trial_frequency: int = 5,
-                 do_ga: bool = False,
-                 do_subsumption: bool = True,
-                 beta: float = 0.05,
-                 gamma: float = 0.95,
-                 theta_i: float = 0.1,
-                 theta_r: float = 0.9,
-                 epsilon: float = 0.5,
-                 biased_exploration: float = 0.5,
-                 cover_noise: float = 0.1,
-                 mutation_noise: float = 0.1,
-                 u_max: int = 100000,
-                 theta_exp: int = 20,
-                 theta_ga: int = 100,
-                 theta_as: int = 20,
-                 mu: float = 0.3,
-                 chi: float = 0.8) -> None:
+    def __init__(self, **kwargs) -> None:
+        self.classifier_length: int = kwargs['classifier_length']
+        self.number_of_possible_actions: int = kwargs[
+            'number_of_possible_actions']
+        self.encoder = kwargs['encoder']
 
-        if encoder is None:
-            raise TypeError('Real number encoder should be passed')
+        self.beta: float = kwargs.get('beta', 0.05)
+        self.gamma: float = kwargs.get('gamma', 0.95)
+        self.theta_i: float = kwargs.get('theta_i', 0.1)
+        self.theta_r: float = kwargs.get('theta_r', 0.9)
+        self.epsilon: float = kwargs.get('epsilon', 0.5)
+        self.biased_exploration: float = kwargs.get('biased_exploration', 0.5)
 
-        self.oktypes = (UBR,)
-        self.encoder = encoder
-
-        self.classifier_length = classifier_length
-        self.number_of_possible_actions = number_of_possible_actions
-        self.classifier_wildcard = UBR(*self.encoder.range)
-
-        self.metrics_trial_frequency = metrics_trial_frequency
-        self.user_metrics_collector_fcn = user_metrics_collector_fcn
-
-        self.do_ga = do_ga
-        self.do_subsumption = do_subsumption
-
-        self.beta = beta
-        self.gamma = gamma
-        self.theta_i = theta_i
-        self.theta_r = theta_r
-        self.epsilon = epsilon
-        self.biased_exploration = biased_exploration
         # Max range of uniform noise distribution that can alter
         # the perception during covering U[0, cover_noise]
-        self.cover_noise = cover_noise
+        self.cover_noise: float = kwargs.get('cover_noise', 0.1)
+        self.u_max: int = kwargs.get('u_max', 100000)
+        self.do_subsumption: bool = kwargs.get('do_subsumption', True)
+        self.do_ga: bool = kwargs.get('do_ga', False)
+        self.mu: float = kwargs.get('mu', 0.3)
 
         # Max range of uniform noise distribution that can broaden the
         # phenotype interval range.
-        self.mutation_noise = mutation_noise
-        self.u_max = u_max
+        self.mutation_noise: float = kwargs.get('mutation_noise', 0.1)
+        self.chi: float = kwargs.get('chi', 0.8)
+        self.theta_exp: int = kwargs.get('theta_exp', 20)
+        self.theta_ga: int = kwargs.get('theta_ga', 100)
+        self.theta_as: int = kwargs.get('theta_as', 20)
 
-        self.theta_exp = theta_exp
-        self.theta_ga = theta_ga
-        self.theta_as = theta_as
+        self.metrics_trial_frequency: int = kwargs.get(
+            'metrics_trial_frequency', 5)
+        self.user_metrics_collector_fcn: Optional[Callable] = kwargs.get(
+            'user_metrics_collector_fcn', None)
+        self.model_checkpoint_freq: Optional[int] = kwargs.get(
+            'model_checkpoint_frequency', None)
 
-        self.mu = mu
-        self.chi = chi
+        self.oktypes = (UBR,)
+        self.classifier_wildcard = UBR(*self.encoder.range)
