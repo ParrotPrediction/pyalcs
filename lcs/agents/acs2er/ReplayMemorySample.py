dd9d9de73ce05339d3ee40fcd1950ae97dad115d
Lukasz Smierzchala <lukasz.smierzchala@fenergo.com>
2022-04-11 13:34:03 +0200

    change replay memory sample to dataclass

--- lcs/agents/acs2er/ReplayMemorySample.py
+++ lcs/agents/acs2er/ReplayMemorySample.py
@@ -1,19 +1,11 @@
 from __future__ import annotations
 from lcs import Perception
+from dataclasses import dataclass
 
-
+@dataclass
 class ReplayMemorySample:
-    __slots__ = ['state', 'action', 'reward', 'next_state', 'done']
-
-    def __init__(self,
-                 state: Perception,
-                 action: int,
-                 reward: float,
-                 next_state: Perception,
-                 done: bool) -> None:
-
-        self.state = state
-        self.action = action
-        self.reward = reward
-        self.next_state = next_state
-        self.done = done
+    state: Perception
+    action: int
+    reward: float
+    next_state: Perception
+    done: bool
